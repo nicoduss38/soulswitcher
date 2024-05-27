f2c4a94eaca63619a9877187c49835e711180709
nicoduss38 <nicolas.dussert.rosset@gmail.com>
2024-05-27 10:45:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const audioPlayer = document.getElementById("audioPlayer");
+    const audioSource = document.getElementById("audioSource");
+    const playlist = document.getElementById("playlist");
+
+    playlist.addEventListener("click", function(event) {
+        if (event.target && event.target.nodeName === "LI") {
+            const src = event.target.getAttribute("data-src");
+            audioSource.src = src;
+            audioPlayer.load();
+            audioPlayer.play();
+        }
+    });
 
     audioPlayer.addEventListener("play", function() {
         console.log("Lecture de la musique");
